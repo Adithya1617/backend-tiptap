3c0391a09bfac19a1d120285aa80ccf5c4c639b1
Adithya <adithya.vardhan1617@gmail.com>
2025-07-08 15:58:23 +0530

    added front end url in cors

--- gemini_canvas_app.py
+++ gemini_canvas_app.py
@@ -42,7 +42,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5173",         # Added for Vite/React dev server
-        "http://127.0.0.1:5173"           # Added for Vite/React dev server
+        "http://127.0.0.1:5173",          # Added for Vite/React dev server
+        "https://i20frontend.vercel.app"  # Added for deployed frontend
     ],  # Specify allowed origins
     allow_credentials=True,
     allow_methods=["GET", "POST"],
